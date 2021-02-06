eb509822d0392ed09eb5b1ec0a1fe4477bb1c27c
Sathishchary <ksathish@xtremand.com>
2021-02-06 17:56:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-zji2jh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-zji2jh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent implements OnInit {
+  name = "Angular " + VERSION.major;
+  updatedArray: any = [];
+  inputArray = [
+    { start: 0, end: 4, price: 5 },
+    { start: 2, end: 8, price: 3 },
+    { start: 7, end: 11, price: 10 }
+  ];
+  /// expected output [{0, 2, 5 }, { 2, 8, 3}, {8, 11, 10}]
+  constructor() {}
+  ngOnInit() {
+    this.modifyCode(this.inputArray);
+  }
+
+  modifyCode(arr) {
+    let value: any;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].start < arr[i].end) {
+        let getValue = arr.filter(item => item.start < arr[i].end);
+        if (getValue.length > 1) {
+          value = getValue.reduce((prev, current) => {
+            if (+current.start > +prev.start) {
+              return current;
+            } else {
+              return prev;
+            }
+          });
+        } else {
+          value = getValue;
+        }
+        console.log(value);
+        this.setValues(arr[i].start, value.start, arr[i].price);
+      }
+    }
+  }
+  setValues(start, end, price) {
+    return this.updatedArray.push({ start: start, end: end, price: price });
+  }
+}
